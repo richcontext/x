7f05b566d9a60f91965b44c10c827a65b18c91bb
Robbie Lankford <rlankford@richcontext.com>
2019-09-25 14:47:10 -0500

    scratch that, setting db.Error instead

--- gormx/datasource.go
+++ gormx/datasource.go
@@ -1,6 +1,7 @@
 package gormx
 
 import (
+	"errors"
 	"sync"
 
 	"github.com/jinzhu/gorm"
@@ -11,22 +12,24 @@ import (
 var db *gorm.DB
 var dbOnce sync.Once
 
-func Datasource() (*gorm.DB, error) {
+func Datasource() *gorm.DB {
 	var err error
 	if db == nil {
 		dbOnce.Do(func() {
 			db, err = gorm.Open(viper.GetString("database.dialect"), viper.GetString("database.url"))
 			if err != nil {
 				log.Error().Msgf("Failed to connect database. Error: %s", err.Error())
+				db.Error = errors.New(err.Error())
 
 			}
 
 			if err = db.DB().Ping(); err != nil {
 				log.Error().Msgf("Failed to connect database. Error: %s", err.Error())
+				db.Error = errors.New(err.Error())
 			}
 		})
 	}
-	return db, err
+	return db
 }
 
 func OverrideDatasource(d *gorm.DB) {
